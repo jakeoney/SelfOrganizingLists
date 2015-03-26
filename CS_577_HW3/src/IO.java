c1d2aca11efabdc6892eb70d86eabec0ebe84e31
jakeoney <jakeoney09@gmail.com>
2015-03-26 01:06:35 -0500

    File IO working well with 1 round, 1 division

---
+++ CS_577_HW3/src/IO.java
@@ -0,0 +1,51 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class IO 
+{
+	private final String midwest[] = {"MIDWEST", "Kentucky", "Kansas", "Notre Dame", "Maryland", "West Virginia", "Butler", "Wichita", 
+			"Cincinnati", "Purdue", "Indiana", "Texas", "Buffalo", "Valparaiso", "Northeastern", "New Mexico State", "Hampton"};
+
+	private int participants[];
+	private int queries[];
+	private int round;
+
+	public IO(int[] participants, int[] queries, int round){
+		this.participants = participants;
+		this.queries = queries;
+		this.round = round;
+	}
+
+	public boolean writeRoundResultsToFile(String filename){
+		System.out.println("Ever in file iO?");
+		boolean writeSuccessful = false;
+		try{
+		BufferedWriter outputWriter = null;
+		outputWriter = new BufferedWriter(new FileWriter(filename));
+		outputWriter.write(midwest[0] + " Round " + round);
+		outputWriter.newLine();
+		outputWriter.newLine();
+		for (int i = 1; i < participants.length; i++) {
+			
+			// write name of team
+			outputWriter.write(midwest[participants[i]] + ", ");
+			
+			outputWriter.write(Integer.toString(queries[i]));
+			outputWriter.newLine();
+		}
+		writeSuccessful = true;
+		outputWriter.flush();  
+		outputWriter.close();
+		} catch (IOException e){
+			System.out.println("Error Writing to File");
+			System.exit(2);
+		} catch (NumberFormatException e){
+			System.out.println("Query number wasn't a number?");
+			System.exit(2);
+		}
+
+		return writeSuccessful;
+	}
+}
--- CS_577_HW3/src/SelfOrganizingList.java
+++ CS_577_HW3/src/SelfOrganizingList.java
@@ -2,71 +2,81 @@
 public class SelfOrganizingList 
 {
 	public static final int NCAA_TOURNAMENT = 1;
-	
+
 	public static String midwest[] = {"", "Kentucky", "Kansas", "Notre Dame", "Maryland", "West Virginia", "Butler", "Wichita", 
 		"Cincinnati", "Purdue", "Indiana", "Texas", "Buffalo", "Valparaiso", "Northeastern", "New Mexico State", "Hampton"};
-	
+
 	public static void main(String[] args){
 		boolean modeMTF = false, modeTranspose = false, modeCount = false;
+		boolean generateNCAAData = false;
 		int scenario = 0;
 		int i = 0;
 		NCAA tournament = null;
-		
+
 		//parse cmd line args
-		if(args.length == 4){
-			while(i < args.length){
-				if(args[i].equals("-m")){
-					i++;
-					if(args[i].equalsIgnoreCase("mtf")){
-						modeMTF = true;
-					}
-					else if(args[i].equalsIgnoreCase("count")){
-						modeCount = true;
-					}
-					else if(args[i].equalsIgnoreCase("transpose")){
-						modeTranspose = true;
-					}
-				}
-				else if(args[i].equals("-s")){
-					i++;
-					if(args[i].equalsIgnoreCase("ncaa")){
-						scenario = NCAA_TOURNAMENT; //corresponds to ncaa
-						tournament = new NCAA();
-					}
-				}
+		while(i < args.length){
+			if(args[i].equals("-m")){
 				i++;
+				if(args[i].equalsIgnoreCase("mtf")){
+					modeMTF = true;
+				}
+				else if(args[i].equalsIgnoreCase("count")){
+					modeCount = true;
+				}
+				else if(args[i].equalsIgnoreCase("transpose")){
+					modeTranspose = true;
+				}
 			}
+			else if(args[i].equals("-s")){
+				i++;
+				if(args[i].equalsIgnoreCase("ncaa")){
+					scenario = NCAA_TOURNAMENT; //corresponds to ncaa
+				}
+			}
+			else if(args[i].equals("-g"))
+			{
+				generateNCAAData = true;
+				//then we will want to generate results for NCAA_tournament
+			}
+			else{
+				System.out.println("Usage flags are:\n"
+						+ "-m <mode>\t for which mode you want to run in, options are mtf, count, transpose\n"
+						+ "-g\t\t to generate new NCAA tournament data\n"
+						+ "-s <scenario>\t for the scenario you want to observe, options are ncaa, ...");
+				System.exit(1);
+			}
+			i++;
 		}
+
 		
 		//pass each query into private self organizing list
 		//rounds 1-6 correspond to Midwest
 		int winners[] = null; //index into the original participants array
 
-		if(scenario == NCAA_TOURNAMENT){
+		if(generateNCAAData){
+			tournament = new NCAA();
 			String participants[] = null;
 			int roundQueries[] = null;
+			String filename = "Round 1 Midwest Results.txt";
 			for(int round = 1; round < 2; round++){
 				if(round == 1){
 					participants = midwest;
 				}
-
-				if(modeMTF){
-					//find the winners for the given round
-					winners = tournament.generateWinners(participants, round);
-					//figure out the queries for each team that round
-					GenerateQuery queries = new GenerateQuery(winners, round);
-					roundQueries = queries.getQueries();
-					//print the names of teams and number of queries per team to a file
-					
-					//reorder the participants such that participants /= 2
-				}
-				else if(modeCount){
-					
-				}
-				else if(modeTranspose){
-					
-				}
+				//find the winners for the given round
+				winners = tournament.generateWinners(participants, round);
+				
+				//figure out the queries for each team that round
+				GenerateQuery queries = new GenerateQuery(winners, round);
+				roundQueries = queries.getQueries();
+				
+				//print the names of teams and number of queries per team to a file
+				IO io = new IO(winners, roundQueries, round);
+				io.writeRoundResultsToFile(filename);
+				
+				//reorder the participants such that participants /= 2
 			}	
+			
+			//TESTING INFO//
 			for(int z = 1; z < 17; z++){
 				if(z == 1){
 					System.out.println("--winners--");
@@ -78,7 +88,19 @@ public class SelfOrganizingList
 				System.out.println(roundQueries[z]);
 			}
 		}
-		//count number of times each team gets generated
+		
+		else if(scenario == NCAA_TOURNAMENT){
+
+			if(modeMTF){
+
+			}
+			else if(modeCount){
+
+			}
+			else if(modeTranspose){
+
+			}
+		}
 
 	}
 
